807331e563bc753f9f6aa49322288e2293274db0
Jonathan Scheiber <jon@scheiber.me>
2022-07-26 02:55:00 -0400

    update TransactionDetails.js

---
+++ src/components/TransactionDetails.js
@@ -0,0 +1,63 @@
+import { useState, useEffect } from "react";
+import { Link, useNavigate, useParams } from "react-router-dom";
+import axios from "axios";
+const API = process.env.REACT_APP_API_URL;
+
+function TransactionDetails() {
+  const [transaction, setTransaction] = useState([]);
+  let { index } = useParams();
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    axios
+      .get(`${API}/transactions/${index}`)
+      .then((res) => {
+        setTransaction(res.data);
+      })
+      .catch(() => {
+        navigate("/error");
+      });
+  }, [index, navigate]);
+
+  const handleDelete = () => {
+    axios
+      .delete(`${API}/transactions/${index}`)
+      .then(() => {
+        navigate("/transactions");
+      })
+      .catch(() => {
+        navigate("/error");
+      });
+  };
+
+  return (
+    <div>
+      <ul className="trans-details">
+        <h2>{transaction.item_name}</h2>
+        <li>Date: {transaction.date}</li>
+        <li>Amount: ${transaction.amount}</li>
+        <li>From: {transaction.from}</li>
+        <li>Category: {transaction.category}</li>
+      </ul>
+      <div className="showNavigation">
+        <div>
+          <Link to={`/transactions`}>
+            <button>Back to transactions</button>
+          </Link>
+        </div>
+        <div>
+          <Link to={`/transactions/${index}/edit`}>
+            <button>Edit transaction</button>
+          </Link>
+        </div>
+        <div>
+          <button className="delete" onClick={handleDelete}>
+            Delete transaction
+          </button>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default TransactionDetails;
